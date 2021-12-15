745260cb18238a3fed43ba4d5b9e98ef36d54484
Evgenzayts <evgenzaytsev2002@yandex.ru>
2021-12-15 12:09:52 +1200

    2.1 (fix checks)

--- include/example.hpp
+++ include/example.hpp
@@ -1,8 +1,8 @@
 // Copyright 2021 Evgenzayts evgenzaytsev2002@yandex.ru
 
-#ifndef INCLUDE_EXAMPLE_HPP
-#define INCLUDE_EXAMPLE_HPP
+#ifndef INCLUDE_EXAMPLE_HPP_
+#define INCLUDE_EXAMPLE_HPP_
 
 auto example() -> void;
 
-#endif  // INCLUDE_EXAMPLE_HPP
+#endif  // INCLUDE_EXAMPLE_HPP_
